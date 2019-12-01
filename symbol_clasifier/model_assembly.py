3f64d1abcf050fb092e966801af1fa0d73fc9ef5
francuz-v <vera.frantsuzova@yandex.ru>
2019-12-01 14:55:27 +0300

    class model assembly: 3 models compare results

---
+++ symbol_clasifier/model_assembly.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import pickle
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+
+class Model_Assembly():
+
+    def __init__(self):
+        with open('model34.obj', 'rb') as f:
+            self.model1 = pickle.load(f)
+        with open('model_34_filt5.obj', 'rb') as f:
+            self.model2 = pickle.load(f)
+        with open('model_34_filt6.obj', 'rb') as f:
+            self.model3 = pickle.load(f)
+
+    def predict(self, X_test):
+        y_1 = self.model1.predict(X_test)
+        y_2 = self.model2.predict(X_test)
+        y_3 = self.model3.predict(X_test)
+        y_predicted = []
+        for row in range(X_test.shape[0]):
+            class1 = np.argmax(y_1[row])
+            class2 = np.argmax(y_2[row])
+            class3 = np.argmax(y_3[row])
+            if class1 == class2 or class2 == class3:
+                y_predicted.append(np.argmax(y_2[row]))
+            else:
+                y_predicted.append(class1)
+        return np.array(y_predicted)
